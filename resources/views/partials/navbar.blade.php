13276c4127555521a4914fa1d817a0c631d581a9
MikoFahreza <mikoyusrizal@gmail.com>
2025-07-04 20:46:38 +0700

    fix navbar toggle

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -13,15 +13,33 @@
   color: white; /* Warna teks saat di-hover */
 }
 
+/* Mobile responsive styles */
+@media (max-width: 991px) {
+  .mobile-auth-buttons {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid #e9ecef;
+  }
+  
+  .mobile-auth-buttons .btn {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+}
 </style>
-<nav class="navbar navbar-expand-lg" style="background-color: #fff; ">
+
+<nav class="navbar navbar-expand-lg" style="background-color: #fff;">
 <div class="container-fluid">
-  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  <!-- Logo -->
   <img src="./images/logo.png" alt="logo" width="104" height="65">
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  
+  <!-- Mobile toggle button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <!-- Navigation Menu -->
     <div class="mx-auto">
       <ul class="navbar-nav mb-2 mb-lg-0 justify-content-center">
         <li class="nav-item">
@@ -36,8 +54,17 @@
       </ul>
     </div>
 
-    <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#loginModal" style="border-radius: 20px; margin-right: 10px; width: 100px; height: 35px;">Sign In</button>
-    <button class="btn btn-primary custom-button-sign-up" onclick="window.location.href='{{ route('signup') }}'">Sign Up</button>
+    <!-- Auth buttons - Desktop -->
+    <div class="d-none d-lg-flex">
+      <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#loginModal" style="border-radius: 20px; margin-right: 10px; width: 100px; height: 35px;">Sign In</button>
+      <button class="btn btn-primary custom-button-sign-up" onclick="window.location.href='{{ route('signup') }}'">Sign Up</button>
+    </div>
+    
+    <!-- Auth buttons - Mobile -->
+    <div class="d-lg-none mobile-auth-buttons">
+      <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#loginModal" style="border-radius: 20px;">Sign In</button>
+      <button class="btn btn-primary custom-button-sign-up" onclick="window.location.href='{{ route('signup') }}'">Sign Up</button>
+    </div>
   </div>
 </div>
 </nav>
@@ -55,11 +82,11 @@
     <!-- Modal Body -->
     <div class="modal-body" style="margin-bottom:100px;">
       <div class="row">
-        <div class="col-lg-6 content-left">
+        <div class="col-lg-6 content-left d-none d-lg-block">
           <img src="./images/sign-in.png" alt="Illustration" style="width: 450px; height: 450px; margin-right: 50px; margin-left: 50px;">
         </div>
         <!-- IMAGE -->
-        <div class="col-lg-6 content-right" style="padding-right: 180px;">
+        <div class="col-lg-6 col-12 content-right" style="padding-right: 15px;">
           <!-- TITLE -->
           <p class="sign-in-modal">Sign In</p>
           <p class="welcome-modal">Welcome back! Please enter<br>your details.</p>
@@ -77,21 +104,14 @@
             </div>
             <!-- BUTTON SIGN IN -->
             <button type="submit" class="btn btn-primary custom-button-sign-in-modal">Sign In</button>
-            {{-- <button class="btn btn-primary custom-button-sign-in-modal" type="submit" value="login"> --}}
           </form>
           <a href="{{ route('google.redirect') }}" class="btn-google mt-3">
             <img src="./images/google.png" style="width: 15px; height: 15px;" alt="Google Icon"> Sign In with Google
           </a>
-          <p class="dont-have-account-modal">Don’t have an account? <span style="color: #1466C2;" onclick="window.location.href='signup'"><a href="javascript:void(0)" style="text-decoration: none;">Sign Up</a></span></p>
+          <p class="dont-have-account-modal">Don't have an account? <span style="color: #1466C2;" onclick="window.location.href='signup'"><a href="javascript:void(0)" style="text-decoration: none;">Sign Up</a></span></p>
         </div>
       </div>
     </div>
-
-    <!-- Modal Footer -->
-    <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div> -->
-
   </div>
 </div>
 </div>
